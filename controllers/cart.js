93889150981f6d1e6edb9be125eab7a201bb146d
Regina Jalandoni <reginajalandoni13@gmail.com>
2024-07-11 12:41:48 +0800

    Update s53 codes

--- controllers/cart.js
+++ controllers/cart.js
@@ -23,95 +23,95 @@ module.exports.getCart = async (req, res) => {
     }
 };
 
+module.exports.addToCart = (req, res) =>{
+    try{
+        const userId = req.user.id;
+        const {productId, quantity} = req.body;
 
-module.exports.addToCart = (req, res) => {
-    console.log(req.user.id);
-    try {
-        // Check if the user has an existing cart
-        Cart.findOne({ userId: req.user.id })
-            .then((cart) => {
-                // If the user has a cart, proceed here:
-                if (cart) {
-                    // Check if the product is already in the cart
-                    const existingCartItem = cart.cartItems.find(item => item.productId === req.body.productId);
-
-                    return Product.findById(req.body.productId)
-                        .then((product) => {
-                            if (!product) {
-                                throw new Error("Product not found");
-                            }
-
-                            if (existingCartItem) {
-                                // Product already exists in cart, update quantity and subtotal
-                                existingCartItem.quantity += req.body.quantity;
-                                existingCartItem.subtotal = existingCartItem.quantity * product.price;
-                            } else {
-                                // Product is not in cart, add it as a new cartItem
-                                const newSubtotal = req.body.quantity * product.price;
-                                cart.cartItems.push({
-                                    productId: req.body.productId,
-                                    quantity: req.body.quantity,
-                                    subtotal: newSubtotal
-                                });
-                            }
-
-                            // Calculate totalPrice based on cartItems
-                            cart.totalPrice = cart.cartItems.reduce((total, item) => total + item.subtotal, 0);
-
-                            // Save the updated cart
-                            return cart.save()
-                                .then((savedCart) => {
-                                    const lastItem = savedCart.cartItems[savedCart.cartItems.length - 1];
-                                    res.status(200).send({
-                                        message: "Item added to cart successfully",
-                                        cart: lastItem,
-                                        totalPrice: savedCart.totalPrice,
-                                        orderedOn: savedCart.orderedOn,
-                                        __v: savedCart.__v
-                                    });
-                                })
-                                .catch(error => errorHandler(error, req, res));
-                        })
-                        .catch(error => errorHandler(error, req, res));
-                } else {
-                    // User does not have a cart, create a new one
-                    return Product.findById(req.body.productId)
-                        .then((product) => {
-                            if (!product) {
-                                throw new Error("Product not found");
-                            }
-
-                            const newSubtotal = req.body.quantity * product.price;
-
-                            let newCart = new Cart({
-                                userId: req.user.id,
-                                cartItems: [{
-                                    productId: req.body.productId,
-                                    quantity: req.body.quantity,
-                                    subtotal: newSubtotal
-                                }],
-                                totalPrice: newSubtotal // Set totalPrice initially based on the first item
-                            });
-
-                            return newCart.save()
-                                .then((cart) => {
-                                    res.status(200).send({
-                                        message: "Item added to cart successfully",
-                                        cart: cart
-                                    });
-                                })
-                                .catch(error => errorHandler(error, req, res));
-                        })
-                        .catch(error => errorHandler(error, req, res));
+        // Find the users cart
+        Cart.findOne({userId: userId})
+        .then((existingCart) =>{
+            return Product.findById(productId)
+            .then((existingProduct) =>{
+                if(!existingProduct){
+                    return res.status(404).send({message: "Product not found"})
                 }
-            })
-            .catch(error => errorHandler(error, req, res));
-    } catch (err) {
+                // if user has already a cart
+                if(existingCart){
+                    // Fin the specific product in the cart
+                    let existingItem = existingCart.cartItems.find(item => item.productId === productId)
+                    
+                    // If the product is existing in the cart
+                    if(existingItem){
+                        // Update the quantity and subtotal
+                        existingItem.quantity += quantity;
+                        existingItem.subtotal = existingItem.quantity * existingProduct.price;
+
+                        // Update the total price
+                        existingCart.totalPrice = existingCart.cartItems.reduce((total, item) => total + item.subtotal, 0);
+
+                        return existingCart.save()
+                        .then((updatedCard) => {
+                            res.status(200).send({
+                                message: "Added to cart successfully",
+                                cart: updatedCard
+                            })
+                        })
+                        .catch(error => errorHandler(error, req, res));
+                    }
+                    // If the product is not existing in the cart
+                    else{
+                        existingCart.cartItems.push({
+                            productId: productId,
+                            quantity: quantity,
+                            subtotal: existingProduct.price * quantity
+                        })
+
+                        // Update the total price
+                        existingCart.totalPrice = existingCart.cartItems.reduce((total, item) => total + item.subtotal, 0);
+
+                        return existingCart.save()
+                        .then((updatedCard) => {
+                            res.status(200).send({
+                                message: "Added to cart successfully",
+                                cart: updatedCard
+                            })
+                        })
+                        .catch(error => errorHandler(error, req, res));
+                    }
+                // If user has no cart
+                }else{
+                    let newSubtotal = existingProduct.price * quantity
+                    let newCart = new Cart({
+                        userId: userId,
+                        cartItems:[
+                            {
+                                productId: productId,
+                                quantity: quantity,
+                                subtotal: newSubtotal
+                            }
+                        ],
+                        totalPrice: newSubtotal
+                    })
+
+                    return newCart.save()
+                    .then((cart) =>{
+                        res.status(200).send({
+                            message: "Added to cart successfully",
+                            cart: cart
+                        })
+                    })
+                    .catch(error => errorHandler(error, req, res));
+                }
+
+            }).catch(error => errorHandler(error, req, res));
+        }).catch(error => errorHandler(error, req, res));
+
+    }catch(err){
         console.error(err);
-        res.status(500).send({ message: "Internal Server Error" });
+        res.status(500).send({message: "Internal Server Error"});
     }
 }
-
 
 
 module.exports.updateCartQuantity = async (req, res) => {
